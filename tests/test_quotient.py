262ae87f0239fea9f82b7fa10aabffbd539da1eb
Yizhar (Izzy) Toren <1861565+ytoren@users.noreply.github.com>
2020-08-30 21:30:36 +0200

    Logic for quotient graph

---
+++ tests/test_quotient.py
@@ -0,0 +1,43 @@
+from numpy import allclose, array
+from scipy.sparse import csr_matrix, issparse
+from pysimscale import merge_row_partition
+
+partition = [[1, 2, 3], [4, 5], [0]]
+
+m = csr_matrix(array([
+    [1.0, 0.9, 0.0, 0.2, 0.0, 0.1],
+    [0.9, 1.0, 0.6, 0.0, 0.0, 0.0],
+    [0.0, 0.6, 1.0, 0.0, 0.5, 0.0],
+    [0.2, 0.0, 0.0, 1.0, 0.0, 0.0],
+    [0.0, 0.0, 0.5, 0.0, 1.0, 0.8],
+    [0.1, 0.0, 0.0, 0.0, 0.8, 1.0]
+]))
+
+m_merge_rows = array([
+    [1.0, 0.9, 0.0, 0.2, 0.0, 0.1],
+    [1.1, 1.6, 1.6, 1.0, 0.5, 0.0],
+    [0.1, 0.0, 0.5, 0.0, 1.8, 1.8]
+])
+
+m_merged = array([
+    [1.0, 1.1, 0.1],
+    [1.1, 4.2, 0.5],
+    [0.1, 0.5, 3.6]
+])
+
+def test_row_merge():
+    assert allclose(merge_row_partition(m, partition, agg='sum').todense(), m_merge_rows)
+
+def test_full_merge():
+    assert allclose(
+        merge_row_partition(
+            merge_row_partition(
+                m,
+                partition,
+                agg='sum'
+            ).T,
+            partition,
+            agg='sum'
+        ).todense(),
+        m_merged
+    )
