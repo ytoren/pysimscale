510fdc6d16fe575f64d88891c18435bab56b26a1
Yizhar (Izzy) Toren <1861565+ytoren@users.noreply.github.com>
2020-09-02 21:21:31 +0200

    Setup file: add deps & classifiers, rewrite readme

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+def reademe():
+    with open("README.md", "r") as fh:
+        return fh.read()
 
 setuptools.setup(
     name="SimScale",
@@ -9,14 +10,22 @@ setuptools.setup(
     author="Yizhar (Izzy) Toren",
     author_email="ytoren+pysimscale@gmail.com",
     description="Large scale similarity matrix calculus (parallelisation & chage of scale)",
-    long_description=long_description,
+    long_description=readme(),
     long_description_content_type="text/markdown",
     url="github.com/ytoren/pysimscale",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Image Processing',
+        'Topic :: Scientific/Engineering :: Information Analysis'
     ],
-    python_requires='>=3.6'
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+        'scipy'
+    ]
 )
