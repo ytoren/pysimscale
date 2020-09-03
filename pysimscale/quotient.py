a38545b40304476fb18aacb1c548b9bc10f8d9ee
Yizhar (Izzy) Toren <1861565+ytoren@users.noreply.github.com>
2020-09-03 22:27:27 +0200

    FIX: unsorted partitions

--- pysimscale/quotient.py
+++ pysimscale/quotient.py
@@ -1,4 +1,4 @@
-from pysimscale import sort_partition, is_partition
+from pysimscale import is_partition
 from scipy.sparse import vstack, csr_matrix
 
 CLASS_STR = ('sum', 'min', 'max', 'mean', 'getnnz')
@@ -17,22 +17,18 @@ def merge_row_partition(m, partition, agg='sum'):
         return vstack([agg(m, p) for p in partition])
 
 
-def quotient_similarity(m, partition, agg='sum', diag_value=None, sort=True, check=True):
+def quotient_similarity(m, partition, agg='sum', diag_value=None, check=False):
     '''Generate quotient similarity matrix based on the given partition of matrix rows
 
     Params:
     - m: A symmetric 2D array (can be sparse) containing the similarity matrix
     - partition: A list-of-lists partitionning the rows/columns of `m`
     - agg: One of ('sum', 'min'. 'max', 'mean', 'getnnz') or a function that takes as parameters the matrix and a range of indices from the partition and aggregates the values across the rows
-    - sort: Logical. Should the partition be sorted? Default is `True`
     - check: Logical. Should the dunction check that `partition` is a valid partition of the rows of m? Default is 'True'
 
     '''
     if not callable(agg) and (isinstance(agg, str) and not agg in CLASS_STR):
         raise ValueError('''Agg has to be one of ('sum', 'min'. 'max', 'mean', 'getnnz') or a function''')
-
-    if sort:
-        partition = sort_partition(partition, min)
 
     if check:
         if not is_partition(partition, start=0, end=m.shape[0]-1):
--- tests/test_quotient.py
+++ tests/test_quotient.py
@@ -1,8 +1,13 @@
+from importlib.util import find_spec
 from numpy import allclose, array
 from scipy.sparse import csr_matrix, issparse
 from pysimscale import merge_row_partition, quotient_similarity, sort_partition
 
-partition = [[1, 2, 3], [4, 5], [0]]
+if find_spec('networkx') is not None:
+    HAS_NX = True
+
+if HAS_NX:
+    from networkx import from_scipy_sparse_matrix, quotient_graph, to_numpy_array, Graph
 
 m = csr_matrix(array([
     [1.0, 0.9, 0.0, 0.2, 0.0, 0.1],
@@ -13,20 +18,41 @@ m = csr_matrix(array([
     [0.1, 0.0, 0.0, 0.0, 0.8, 1.0]
 ]))
 
+partition = [[2], [1, 0, 3], [4, 5]]
+
 m_merge_rows = array([
-    [1.0, 0.9, 0.0, 0.2, 0.0, 0.1],
-    [1.1, 1.6, 1.6, 1.0, 0.5, 0.0],
+    [0.0, 0.6, 1.0, 0.0, 0.5, 0.0],
+    [2.1, 1.9, 0.6, 1.2, 0.0, 0.1],
     [0.1, 0.0, 0.5, 0.0, 1.8, 1.8]
 ])
 
 m_merged = array([
-    [1.0, 1.1, 0.1],
-    [1.1, 4.2, 0.5],
-    [0.1, 0.5, 3.6]
+    [1.0, 0.6, 0.5],
+    [0.6, 5.2, 0.1],
+    [0.5, 0.1, 3.6]
 ])
 
+
 def test_row_merge():
-    assert allclose(merge_row_partition(m, sort_partition(partition), agg='sum').todense(), m_merge_rows)
+    assert allclose(merge_row_partition(m, partition, agg='sum').todense(), m_merge_rows)
 
 def test_full_merge():
     assert allclose(quotient_similarity(m, partition, agg='sum').todense(), m_merged)
+
+if HAS_NX:
+    G = from_scipy_sparse_matrix(m)
+
+    def edge_sum(u, v):
+        Guv = Graph(G.subgraph(list(u) + list(v)))
+        Guv.remove_edges_from(G.subgraph(list(u)).edges)
+        Guv.remove_edges_from(G.subgraph(list(v)).edges)
+        return {'weight': Guv.size(weight='weight')}
+
+    def node_sum(u):
+        return {'weight': G.subgraph(list(u)).size(weight='weight')}
+
+    def test_nx():
+        mq1 = quotient_similarity(m, partition, agg='sum', diag_value=0).todense()
+        Gq = quotient_graph(G=G, partition=partition, edge_data=edge_sum)
+        mq2 = to_numpy_array(Gq)
+        assert allclose(mq1, mq2)
